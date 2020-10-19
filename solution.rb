a18ce318c0fd741c0fd612106960c115abbfa9a5
Stephen Ibanez <sibanez93@Stephens-MacBook-Pro-3.local>
2020-10-19 10:25:40 -0700

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,38 @@
+require 'pry'
+
 class Deck
+
+    attr_reader :cards, :rank, :suit
+
+    def initialize
+        @cards = []
+        for suit in ["Hearts", "Clubs", "Diamonds", "Spades"] do     
+        for rank in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"] do
+                @cards << Card.new(suit, rank)
+            end
+        end
+    end
+
+    def choose_card
+        # @cards.sample.index
+        # binding.pry
+        # @cards.sample.delete
+        # @cards.sample
+        # @cards.sample
+        @cards.delete_at(rand(@cards.length))
+        # @cards.size -= 1
+    end
 
 end
 
 class Card
+
+    attr_reader :rank, :suit
+
+    def initialize (suit , rank)
+        @suit, @rank = suit, rank 
+        # @@all << self
+    end
+    
 
 end
---
+++ spec/deck.rb
@@ -0,0 +1,7 @@
+class Deck
+
+    def initialize(rank: , number: )
+        @rank, @number = rank, number
+    end
+
+end
